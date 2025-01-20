3b9d1d621fbf5b3beb311b9a4951675126fa0e02
RaphaelIniesta <raphael_iniesta@yahoo.com>
2025-01-20 01:46:36 -0300

    Initial Commit

---
+++ CleanHouse/CleanHouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanHouseApp.swift
+//  CleanHouse
+//
+//  Created by Raphael Iniesta Reis on 20/01/25.
+//
+
+import SwiftUI
+
+@main
+struct CleanHouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
